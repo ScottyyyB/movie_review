410eb65e7dd655a5fa4ef49f3b7ebfe83bd85253
ScottB <17sb@cis.dk>
2018-02-27 21:53:16 +0100

    Add user destroy session (sign out) request spec

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'User Sessions', type: :request do
   let(:user) { FactoryBot.create(:user) }
-  let(:headers) {{ HTTP_ACCEPT: 'application/json' }}
+  let(:credentials) { user.create_new_auth_token }
+  let(:headers) { { HTTP_ACCEPT: 'application/json'}.merge!(credentials) }
+
 
   describe 'POST /api/v1/auth/sign_in' do
     it 'should create session for user' do
@@ -35,7 +37,20 @@ RSpec.describe 'User Sessions', type: :request do
       expect(response.status).to eq 401
       expect(response_json['errors']).to eq ['Invalid login credentials. Please try again.']
     end
+  end
 
+  describe 'DELETE /api/v1/auth/sign_out' do
+    before do
+      login_as(user, scope: :user)
+    end
 
+    it 'should sign out user' do
+      delete '/api/v1/auth/sign_out', params: {
+        user: user
+      }, headers: headers
+
+      expect(response.status).to eq 200
+      expect(response_json['success']).to eq true
+    end
   end
 end
